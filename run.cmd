28f1aecc030751e0ddb48521c0523a80899d709f
Kevin Thompson <kevin.thompson@theautomaters.com>
2017-02-06 14:42:09 -0800

    Add gdb and qemu to source tree for easier building.

--- run.cmd
+++ run.cmd
@@ -3,13 +3,15 @@
 set DEBUG_GDB=0
 set QEMU_OPTIONS=
 set ROOT=%~dp0
+set TOOLS_PATH=%ROOT%\tools
 set SCRIPTS_PATH=%ROOT%\scripts
 set BIN_PATH=%ROOT%\bin\Debug
 set ISO_PATH=%BIN_PATH%\os.iso
 set KERNEL_PATH=%BIN_PATH%\kernel.bin
 set KERNEL_PDB=%BIN_PATH%\kernel.pdb
 
-set GDB_CMD="C:\ProgramData\Microsoft\AndroidNDK\android-ndk-r10e\toolchains\x86_64-4.9\prebuilt\windows\bin\x86_64-linux-android-gdb.exe"
+set GDB_CMD=%TOOLS_PATH%\gdb.exe
+set QEMU_CMD=%TOOLS_PATH%\qemu\qemu-system-x86_64.exe
 
 IF "debug"=="%1" SET DEBUG_GDB=1
 
@@ -25,5 +27,5 @@ goto:eof
   )
 
   echo :: starting qemu ::
-  "C:\Program Files\qemu\qemu-system-x86_64.exe" -cdrom "%ISO_PATH%" -no-reboot -d cpu_reset -boot d -m 128 -serial file:serial.log %QEMU_OPTIONS%
+  "%QEMU_CMD%" -cdrom "%ISO_PATH%" -no-reboot -d cpu_reset -boot d -m 128 -serial file:serial.log %QEMU_OPTIONS%
 goto:eof
